9d37e0923781e804f06ad5b97332fc3c6eb73513
amnah.a <amnah44ali@gmail.com>
2022-11-29 21:07:11 +0300

    add dependency of hilt and view model

--- core/ui/src/main/java/com/devfalah/ui/screen/HomeScreen.kt
+++ core/ui/src/main/java/com/devfalah/ui/screen/HomeScreen.kt
@@ -3,24 +3,28 @@ package com.devfalah.ui.screen
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.devfalah.ui.composable.CreatePostItem
+import com.devfalah.viewmodels.home.HomeViewModel
 
 @Composable
 fun HomeScreen(
+    state: HomeViewModel = hiltViewModel()
 ) {
     HomeContent()
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun HomeContent() {
-    LazyColumn{
-       item{
-           CreatePostItem(
-               image = "",
-               value = "hello",
-               onTextChange = {}
-           )
-       }
+fun HomeContent(
+) {
+    LazyColumn {
+        item {
+            CreatePostItem(
+                image = "",
+                value = "hello",
+                onTextChange = {}
+            )
+        }
     }
 }
