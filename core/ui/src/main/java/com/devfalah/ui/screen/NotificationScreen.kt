dca7bdac8f989e695a6b369ea4a0b46edeefde75
Shahad161 <shahad.hamza.014@gmail.com>
2022-11-30 17:55:33 +0300

    Create NotificationScreen

---
+++ core/ui/src/main/java/com/devfalah/ui/screen/NotificationScreen.kt
@@ -0,0 +1,16 @@
+package com.devfalah.ui.screen
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NotificationScreen(){
+
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen(){
+    NotificationScreen()
+}
