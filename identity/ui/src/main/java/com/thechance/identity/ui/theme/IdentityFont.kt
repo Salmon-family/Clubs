c0eeb2feaac541a4cb917da5f0befe4784779f88
mahmoud abou-elhamd <mahmoudabohassab00@gmail.com>
2022-11-30 02:43:37 +0200

    add text styles

---
+++ identity/ui/src/main/java/com/thechance/identity/ui/theme/IdentityFont.kt
@@ -0,0 +1,18 @@
+package com.thechance.identity.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.thechance.identity.ui.R
+
+val PlusJakartaSans = FontFamily(
+    Font(R.font.plus_jakarta_sans_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.plus_jakarta_sans_regular, weight = FontWeight.Normal),
+)
+
+val Inter = FontFamily(
+    Font(R.font.inter_bold, weight = FontWeight.Bold),
+    Font(R.font.inter_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.inter_medium, weight = FontWeight.Medium)
+)
+
