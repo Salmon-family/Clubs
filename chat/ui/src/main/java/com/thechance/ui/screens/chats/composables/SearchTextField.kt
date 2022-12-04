2770e1b69504d7c87f8f97158e3f47a8fc19ae50
Falah Hassan <devfalahhassan@gmail.com>
2022-12-04 19:34:21 +0300

    remove underline from text field

--- chat/ui/src/main/java/com/thechance/ui/screens/chats/composables/SearchTextField.kt
+++ chat/ui/src/main/java/com/thechance/ui/screens/chats/composables/SearchTextField.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -28,8 +29,10 @@ fun SearchTextField(
         modifier = modifier.fillMaxWidth(),
         colors = TextFieldDefaults.textFieldColors(
             backgroundColor = White,
-            focusedIndicatorColor = White,
-            unfocusedIndicatorColor = White,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+
         ),
         value = text,
         shape = RoundedCornerShape(100.dp),
