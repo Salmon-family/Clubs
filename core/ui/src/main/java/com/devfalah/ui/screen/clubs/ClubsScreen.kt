29e4a704d9caba370df67f6fc8d6cbadedecc9d7
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-12-04 17:46:47 +0300

    create search club text field

--- core/ui/src/main/java/com/devfalah/ui/screen/clubs/ClubsScreen.kt
+++ core/ui/src/main/java/com/devfalah/ui/screen/clubs/ClubsScreen.kt
@@ -1,84 +1,15 @@
 package com.devfalah.ui.screen.clubs
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.devfalah.ui.R
-import com.devfalah.ui.spacer.HeightSpacer8
-import com.devfalah.ui.theme.*
 
 @Composable
 fun ClubsScreen(
     navController: NavController,
 ) {
-    ClubCard()
 
-}
-
-@Composable
-fun ClubCard() {
-    Card(elevation = 0.dp,
-        shape = RoundedCornerShape(16.dp ),
-    backgroundColor = Color(0xFFFAFAFA)
-    ) {
-        Column() {
-
-            Image(painter = painterResource(id = R.drawable.user_image),
-                contentDescription = "club cover",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxWidth()
-                    .height(174.dp))
-
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)) {
-
-                Text(text = "The Chance Community",
-                    style = Typography.subtitle2,
-                    color = LightSecondaryBlackColor)
-
-                Text(text = "10K member . 100K post",
-                    style = Typography.caption,
-                    color = LightTernaryBlackColor)
-
-                HeightSpacer8()
-
-                Text(text = "This club is for the community of The Chance, to make discussions about Android development, an...",
-                    style = Typography.caption,
-                    color = LightSecondaryBlackColor)
-
-                HeightSpacer8()
-
-                Button(onClick = { },
-                    modifier = Modifier
-                        .height(38.dp)
-                        .fillMaxWidth(),
-                    shape = RoundedCornerShape(100),
-                    elevation = ButtonDefaults.elevation(0.dp),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = LightBackgroundColor),
-                    border = BorderStroke(1.dp, LightQuaternaryBlackColor )) {
-
-                    Text(text = "Joined Club",
-                        style = Typography.subtitle2,
-                        color = LightTernaryBlackColor)
-
-                }
-            }
-
-        }
-
-    }
 }
 
 @Preview
---
+++ core/ui/src/main/java/com/devfalah/ui/screen/clubs/composable/SearchTextField.kt
@@ -0,0 +1,58 @@
+package com.devfalah.ui.screen.clubs.composable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.devfalah.ui.R
+import com.devfalah.ui.theme.LightCardBackgroundColor
+import com.devfalah.ui.theme.LightTernaryBlackColor
+import com.devfalah.ui.theme.Typography
+
+@Composable
+fun SearchTextField(
+    value : String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = { onValueChanged(it) },
+        singleLine = true,
+        shape = RoundedCornerShape(100),
+        leadingIcon = {
+            Image(
+                painter = painterResource(id = R.drawable.ic_search_outline),
+                contentDescription = "search icon"
+            )
+
+        },
+
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = LightCardBackgroundColor,
+            focusedIndicatorColor = Color.White,
+            unfocusedIndicatorColor = Color.White,
+        ),
+
+        placeholder = {
+            Text(text = "Search for a club",
+                style = Typography.body2,
+                color = LightTernaryBlackColor
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    SearchTextField(value = "", onValueChanged = {})
+}
