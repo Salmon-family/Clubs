96a498e8148450c4a6d74e006772d80b68ddb0ab
Nada Feteiha <nada.feteiha@gmail.com>
2022-12-09 03:46:10 -0800

    fix profile ui

--- core/ui/src/main/java/com/devfalah/ui/screen/profile/composable/FriendsSection.kt
+++ core/ui/src/main/java/com/devfalah/ui/screen/profile/composable/FriendsSection.kt
@@ -1,22 +1,20 @@
 package com.devfalah.ui.screen.profile.composable
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.devfalah.ui.R
+import com.devfalah.ui.composable.CircleProfileImage
 import com.devfalah.ui.composable.HeightSpacer8
 import com.devfalah.ui.theme.LightPrimaryBlackColor
 import com.devfalah.ui.theme.LightPrimaryBrandColor
@@ -39,6 +37,7 @@ fun FriendsSection(
                 text = stringResource(R.string.friends),
                 fontFamily = PlusJakartaSans,
                 color = LightPrimaryBlackColor,
+                fontWeight = FontWeight.SemiBold,
                 fontSize = 14.sp
             )
             Text(
@@ -81,25 +80,22 @@ fun Friend(
     textColor: Color = LightPrimaryBlackColor
 ) {
     Column(
-        modifier = Modifier.width(56.dp),
+        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
+
+        CircleProfileImage(
             painter = painter,
-            contentScale = ContentScale.Crop,
-            contentDescription = null,
-            modifier = modifier
-                .size(56.dp)
-                .clip(RoundedCornerShape(16.dp))
+            size = 56
         )
-
         HeightSpacer8()
-
         Text(
             text = text,
+            modifier = Modifier.sizeIn(maxWidth = 56.dp),
             color = textColor,
             fontFamily = PlusJakartaSans,
-            fontSize = 12.sp
+            fontSize = 12.sp,
+            maxLines = 1
         )
     }
 }
