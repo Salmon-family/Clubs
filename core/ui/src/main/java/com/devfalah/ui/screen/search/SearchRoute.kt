64c42aa00d5c663bbdb94f8cb9c02acd440bc5a1
Nada Feteiha <nada.feteiha@gmail.com>
2022-12-15 00:53:47 -0800

    remove empty class

--- core/ui/src/main/java/com/devfalah/ui/screen/search/SearchRoute.kt
+++ core/ui/src/main/java/com/devfalah/ui/screen/search/SearchRoute.kt
@@ -4,12 +4,9 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 
-class SearchRoute {
-}
-
 private const val ROUTE_SEARCH = "search"
-fun NavGraphBuilder.searchRoute(navController: NavController){
-    composable(route = ROUTE_SEARCH){
+fun NavGraphBuilder.searchRoute(navController: NavController) {
+    composable(route = ROUTE_SEARCH) {
         SearchScreen(navController)
     }
 }
