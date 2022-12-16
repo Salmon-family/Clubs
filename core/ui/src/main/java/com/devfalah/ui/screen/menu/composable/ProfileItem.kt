7ae95468d460f2e8a93e9e8c8461e3890c2254c3
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-12-16 20:47:41 +0300

    edit profile item in menu

--- core/ui/src/main/java/com/devfalah/ui/screen/menu/composable/ProfileItem.kt
+++ core/ui/src/main/java/com/devfalah/ui/screen/menu/composable/ProfileItem.kt
@@ -1,8 +1,11 @@
 package com.devfalah.ui.screen.menu.composable
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
@@ -10,20 +13,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.devfalah.ui.theme.LightCardBackgroundColor
-import com.devfalah.ui.theme.LightPrimaryBlackColor
-import com.devfalah.ui.theme.LightSecondaryBlackColor
+import com.devfalah.ui.R
+import com.devfalah.ui.theme.LightBackgroundColor
+import com.devfalah.ui.theme.LightPrimaryBrandColor
 import com.devfalah.ui.theme.PlusJakartaSans
 
 @Composable
-fun MenuItem(
+fun ProfileItem(
     modifier: Modifier = Modifier,
-    text: String,
     painter: Painter,
     onClickItem: () -> Unit
 ) {
@@ -32,26 +36,31 @@ fun MenuItem(
             .fillMaxWidth()
             .height(56.dp)
             .clip(RoundedCornerShape(28.dp))
-            .background(LightCardBackgroundColor)
+            .background(LightPrimaryBrandColor)
             .clickable { onClickItem() }
             .padding(horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Text(
-            text = text,
+            text = stringResource(R.string.my_account),
             style = TextStyle(
                 fontSize = 14.sp,
                 fontFamily = PlusJakartaSans,
                 fontWeight = FontWeight.SemiBold,
-                color = LightPrimaryBlackColor
+                color = LightBackgroundColor
             )
         )
 
         Icon(
-            modifier = Modifier.size(24.dp),
+            modifier = Modifier
+                .size(32.dp)
+                .border(
+                    BorderStroke(2.dp, LightBackgroundColor),
+                    CircleShape
+                ),
             painter = painter,
-            tint = LightSecondaryBlackColor,
+            tint = Color.Unspecified,
             contentDescription = null
         )
     }
