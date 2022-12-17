f4fe5f9593be70e04c847145bec60cc7b00c9aee
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2022-12-17 23:24:10 +0300

    update menu ui

--- core/ui/src/main/java/com/devfalah/ui/screen/menu/MenuScreen.kt
+++ core/ui/src/main/java/com/devfalah/ui/screen/menu/MenuScreen.kt
@@ -5,20 +5,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.devfalah.ui.R
 import com.devfalah.ui.Screen
 import com.devfalah.ui.screen.menu.composable.AccountSection
+import com.devfalah.ui.screen.menu.composable.MenuItem
 import com.devfalah.ui.screen.menu.composable.PreferencesSection
 import com.devfalah.ui.screen.menu.composable.TopSection
 import com.devfalah.ui.screen.profile.navigateToProfile
 import com.devfalah.ui.theme.LightBackgroundColor
+import com.devfalah.ui.theme.LightTernaryBlackColor
+import com.devfalah.ui.theme.PlusJakartaSans
 import com.devfalah.viewmodels.menu.MenuViewModel
 import com.devfalah.viewmodels.menu.UserUiState
 
@@ -36,7 +47,8 @@ fun MenuScreen(
         onClickAccountSettings = { navController.navigate(route = Screen.AccountSettings.screen_route) },
         onClickFriendsRequests = { navController.navigate(route = Screen.FriendRequestRoute.screen_route) },
         onClickTheme = {},
-        onClickLanguage = {}
+        onClickLanguage = {},
+        onClickReportBug = {}
     )
 }
 
@@ -48,14 +60,16 @@ fun MenuContent(
     onClickAccountSettings: () -> Unit,
     onClickFriendsRequests: () -> Unit,
     onClickTheme: () -> Unit,
-    onClickLanguage: () -> Unit
+    onClickLanguage: () -> Unit,
+    onClickReportBug: () -> Unit
 ) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .background(LightBackgroundColor)
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         item {
@@ -79,6 +93,26 @@ fun MenuContent(
                 onClickLanguage = onClickLanguage
             )
         }
+
+        item {
+            MenuItem(
+                text = stringResource(R.string.report_bugs),
+                painter = painterResource(id = R.drawable.ic_menu_bug),
+                onClickItem = onClickReportBug
+            )
+        }
+
+        item {
+            Text(
+                text = "version would be shown here",
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    color = LightTernaryBlackColor,
+                    fontFamily = PlusJakartaSans,
+                    fontWeight = FontWeight.Normal
+                )
+            )
+        }
     }
 }
 
@@ -86,5 +120,5 @@ fun MenuContent(
 @Preview(showSystemUi = true)
 @Composable
 fun PreviewMenu() {
-    MenuContent(UserUiState(), {}, {}, {}, {}, {}, {})
+    MenuContent(UserUiState(), {}, {}, {}, {}, {}, {}, {})
 }
