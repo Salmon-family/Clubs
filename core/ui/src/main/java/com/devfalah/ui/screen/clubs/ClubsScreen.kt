2ff088407177a9984f876586fbb8d623732dc676
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-12-20 18:33:25 +0300

    add content animation

--- core/ui/src/main/java/com/devfalah/ui/screen/clubs/ClubsScreen.kt
+++ core/ui/src/main/java/com/devfalah/ui/screen/clubs/ClubsScreen.kt
@@ -1,5 +1,7 @@
 package com.devfalah.ui.screen.clubs
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -7,11 +9,10 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -19,10 +20,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.devfalah.ui.R
 import com.devfalah.ui.screen.clubs.composable.MyClubCard
-import com.devfalah.ui.screen.clubs.composable.SearchItem
 import com.devfalah.ui.screen.clubs.composable.SpecialClubItem
 import com.devfalah.ui.theme.LightCardBackgroundColor
 import com.devfalah.ui.theme.LightPrimaryBlackColor
@@ -31,6 +30,8 @@ import com.devfalah.ui.theme.Typography
 import com.devfalah.viewmodels.myClubs.ClubsState
 import com.devfalah.viewmodels.myClubs.MyClubsUiState
 import com.devfalah.viewmodels.myClubs.MyClubsViewModel
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
 
 @Composable
 fun ClubsScreen(
@@ -43,15 +44,43 @@ fun ClubsScreen(
 
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun ClubsContent(
     state: MyClubsUiState,
     onClubClick: (ClubsState) -> Unit
 ) {
+    var animationState by remember { mutableStateOf(false) }
 
-    SpecialClubsScreen(state = state, onClubClick = onClubClick)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp)
+            .background(LightCardBackgroundColor)
+    ) {
+        Button(
+            onClick = { animationState = !animationState },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "click me")
+        }
+
+        AnimatedContent(targetState = animationState,
+            transitionSpec = {
+                slideInHorizontally(tween(300)) with fadeOut(tween(300))
+            }
+
+        ) {
+            when (it) {
+                true -> SpecialClubsScreen(state = state, onClubClick = onClubClick)
+                false -> MyClubsScreen(state = state, onClubClick = onClubClick)
+            }
+        }
+    }
+
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun MyClubsScreen(
     state: MyClubsUiState,
@@ -64,13 +93,14 @@ fun MyClubsScreen(
         contentPadding = PaddingValues(vertical = 16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-
         item {
-            SearchItem(
-                onSearchItemClick = {},
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
+            HorizontalPager(count = 2) {
+
+            }
+
         }
+
+
 
         item {
             Row(modifier = Modifier.padding(horizontal = 16.dp)) {
@@ -126,14 +156,17 @@ fun SpecialClubsScreen(
     state: MyClubsUiState,
     onClubClick: (ClubsState) -> Unit
 ) {
-    LazyVerticalGrid(columns = GridCells.Fixed(count = 3),
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(count = 3),
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier.fillMaxSize()
-        ){
+        modifier = Modifier
+            .fillMaxSize()
+            .background(LightCardBackgroundColor),
+    ) {
 
-        items(9){
+        items(9) {
             SpecialClubItem(iconId = R.drawable.art_icon, clubTitle = "art")
         }
     }
