ad0ce9bfa45e5b3fe9d0ffd6db4f0f941772e4fe
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-12-23 04:27:43 +0300

    save userId in data store after login is success

--- identity/useCases/src/main/java/com/thechance/identity/usecases/LoginUseCase.kt
+++ identity/useCases/src/main/java/com/thechance/identity/usecases/LoginUseCase.kt
@@ -4,10 +4,13 @@ import com.thechance.identity.entities.User
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
-    private val identityRepository: IdentityRepository
+    private val identityRepository: IdentityRepository,
+    private val saveUserId: SaveUserIdUseCase,
 ) {
 
     suspend operator fun invoke(userName: String, password: String): User {
-        return identityRepository.login(userName, password)
+        val user = identityRepository.login(userName, password)
+        saveUserId(user.guid.toString())
+        return user
     }
 }
