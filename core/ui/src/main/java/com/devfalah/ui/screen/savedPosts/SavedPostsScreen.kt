e49baf3d04cf25fb79e61100aac5f65e4bd5cdd8
Nada Feteiha <nada.feteiha@gmail.com>
2022-12-25 00:52:14 -0800

    add navigation from saved threads' posts.

--- core/ui/src/main/java/com/devfalah/ui/screen/savedPosts/SavedPostsScreen.kt
+++ core/ui/src/main/java/com/devfalah/ui/screen/savedPosts/SavedPostsScreen.kt
@@ -2,6 +2,7 @@ package com.devfalah.ui.screen.savedPosts
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,13 +13,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.devfalah.ui.R
+import com.devfalah.ui.composable.AppBar
 import com.devfalah.ui.composable.PostItem
 import com.devfalah.ui.composable.setStatusBarColor
 import com.devfalah.ui.screen.home.openBrowser
-import com.devfalah.ui.screen.postCreation.CREATE_POST_SCREEN
+import com.devfalah.ui.screen.postDetails.navigateToPostDetails
 import com.devfalah.ui.screen.profile.navigateToProfile
 import com.devfalah.ui.theme.LightBackgroundColor
 import com.devfalah.viewmodels.savedPosts.SavedPostUIState
@@ -38,8 +42,17 @@ fun SavedPostsScreen(
 
     SavedPostsContent(
         state = state,
+        navController = navController,
         onClickLike = viewModel::onClickLike,
-        onClickComment = { navController.navigate(CREATE_POST_SCREEN) },
+        onClickComment = {
+            navController.navigateToPostDetails(
+                id = it.postId,
+                isSaved = true,
+                publisherId = it.publisherId,
+                publisherName = it.publisherName,
+                publisherUrl = it.publisherImage
+            )
+        },
         onClickRemoveSavedPost = viewModel::onClickRemoveSavedPost,
         onClickProfile = { navController.navigateToProfile(it) },
         onOpenLinkClick = { openBrowser(context, it) }
@@ -56,35 +69,43 @@ fun SavedPostsScreen(
 @Composable
 fun SavedPostsContent(
     state: SavedPostUIState,
+    navController: NavController,
     onClickLike: (PostUIState) -> Unit,
     onClickComment: (PostUIState) -> Unit,
     onClickRemoveSavedPost: (PostUIState) -> Unit,
     onClickProfile: (Int) -> Unit,
     onOpenLinkClick: (String) -> Unit
 ) {
+    Column(Modifier.fillMaxSize()) {
 
-    LazyColumn(
-        modifier = Modifier
-            .background(LightBackgroundColor)
-            .fillMaxSize(),
-        contentPadding = PaddingValues(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
+        AppBar(
+            title = stringResource(id = R.string.saved_threads),
+            navHostController = navController
+        )
 
-        items(state.posts) {
-            PostItem(
-                state = it,
-                isContentExpandable = true,
-                isMyPost = it.publisherId == state.userId,
-                isClubPost = it.isFromClub,
-                showGroupName = true,
-                onClickLike = { onClickLike(it) },
-                onClickComment = { onClickComment(it) },
-                onClickSave = { onClickRemoveSavedPost(it) },
-                onClickProfile = onClickProfile,
-                onOpenLinkClick = onOpenLinkClick,
-                onClickPostSetting = { },
-            )
+        LazyColumn(
+            modifier = Modifier
+                .background(LightBackgroundColor)
+                .fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+            items(state.posts) {
+                PostItem(
+                    state = it,
+                    isContentExpandable = true,
+                    isMyPost = it.publisherId == state.userId,
+                    isClubPost = it.isFromClub,
+                    showGroupName = true,
+                    onClickLike = { onClickLike(it) },
+                    onClickComment = { onClickComment(it) },
+                    onClickSave = { onClickRemoveSavedPost(it) },
+                    onClickProfile = onClickProfile,
+                    onOpenLinkClick = onOpenLinkClick,
+                    onClickPostSetting = { },
+                )
+            }
         }
     }
 }
