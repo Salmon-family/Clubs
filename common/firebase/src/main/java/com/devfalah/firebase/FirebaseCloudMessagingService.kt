2ff561cca8a49be406e12e20fd9e4c9ee9670c6c
Falah Hassan <devfalahhassan@gmail.com>
2023-01-04 21:06:18 +0300

    remove unused code

--- common/firebase/src/main/java/com/devfalah/firebase/FirebaseCloudMessagingService.kt
+++ common/firebase/src/main/java/com/devfalah/firebase/FirebaseCloudMessagingService.kt
@@ -1,11 +1,6 @@
 package com.devfalah.firebase
 
 
-
-import android.content.Intent
-import android.util.Log
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.ProcessLifecycleOwner
 import com.devfalah.repository.models.NotificationDataModel
 import com.devfalah.repository.models.NotificationKeys
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -29,7 +24,10 @@ class FirebaseCloudMessagingService : FirebaseMessagingService() {
                 val friendId = (data[NotificationKeys.FRIEND_ID_KEY]?.toInt() ?: 0)
                 val title = data[NotificationKeys.TITLE].toString()
                 val description = data[NotificationKeys.DESCRIPTION].toString()
-                notificationService.showNotification(applicationContext,friendId, title, description)
+                notificationService.showNotification(applicationContext,
+                    friendId,
+                    title,
+                    description)
                 GlobalScope.launch {
                     events.emit(
                         NotificationDataModel(
@@ -46,23 +44,9 @@ class FirebaseCloudMessagingService : FirebaseMessagingService() {
         newToken = token
     }
 
-    override fun handleIntent(intent: Intent?) {
-        super.handleIntent(intent)
-        if (isAppOnForeground()){
-            Log.e("DEVFALAH",intent.toString())
-        }else{
-            Log.e("DEVFALAHNOT",intent.toString())
-        }
-
-
-    }
     companion object {
         val events = MutableSharedFlow<NotificationDataModel>()
         var newToken: String = ""
     }
 
-    private fun isAppOnForeground(): Boolean {
-        return ProcessLifecycleOwner.get().lifecycle.currentState
-            .isAtLeast(Lifecycle.State.STARTED)
-    }
 }
