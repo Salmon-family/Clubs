ab28e3e57cbceeb9dcd57d6465580bc552e54d31
Mustafa Ahmed <mstfyjmalalden@gmail.com>
2023-01-07 00:43:50 +0300

    improve top app bar ui

--- core/ui/src/main/java/com/devfalah/ui/composable/AppBar.kt
+++ core/ui/src/main/java/com/devfalah/ui/composable/AppBar.kt
@@ -2,15 +2,20 @@ package com.devfalah.ui.composable
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.devfalah.ui.R
+import com.devfalah.ui.theme.PlusJakartaSans
 
 @Composable
 fun AppBar(
@@ -26,7 +31,14 @@ fun AppBar(
     TopAppBar(
         title = {
             Text(
+                modifier = Modifier.offset(x = if (showBackButton) (-12).dp else 0.dp),
                 text = title,
+                style = TextStyle(
+                    color = MaterialTheme.colors.primaryVariant,
+                    fontFamily = PlusJakartaSans,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
             )
         },
         navigationIcon = if (showBackButton) {
